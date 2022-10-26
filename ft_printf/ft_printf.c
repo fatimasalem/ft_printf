c67906f2e1ddb7d270ea9d052e006a489abbe5be
Fatima Salem <106735363+FatimaSalem@users.noreply.github.com>
2022-10-26 23:07:50 +0400

    Add files via upload
    This project implements the functionality of the built in command printf.
    This project is handling the following conversions:
    - %s (string)
    - %u (unsigned decimal)
    - %i (integer)
    - %d (decimal)
    - %p (pointer)
    - %x (lower case hexadecimal)
    - %X (upper case hexadecimal)
    - %c (character)
    - %% (percentage)
    
    , Also a main file is uploaded to check, test and compare my project conversions to the main printf built in command output to display the result similarities.

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fsalem <fsalem@student.42abudhabi.ae>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/22 14:33:00 by fsalem            #+#    #+#             */
+/*   Updated: 2022/10/26 14:11:17 by fsalem           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+int	ft_spot(va_list arg, char f)
+{
+	int	len;
+
+	len = 0;
+	if (f == 'c')
+		len += ft_character(va_arg(arg, int));
+	if (f == '%')
+		len += ft_character('%');
+	else if (f == 's')
+		len += ft_string(va_arg(arg, char *));
+	else if (f == 'x' || f == 'X')
+		len += ft_hexa(va_arg(arg, unsigned int), f);
+	else if (f == 'i' || f == 'd')
+		len += ft_putnbr(va_arg(arg, int));
+	else if (f == 'p')
+		len += ft_pointer(va_arg(arg, unsigned long));
+	else if (f == 'u')
+		len += ft_unsigned(va_arg(arg, int));
+	return (len);
+}
+
+int	ft_printf(const char *format, ...)
+{
+	int		len;
+	int		i;
+	va_list	arg;
+
+	len = 0;
+	i = 0;
+	va_start(arg, format);
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			len += ft_spot(arg, format[i + 1]);
+			i++;
+		}
+		else
+			len += write(1, &format[i], 1);
+		i++;
+	}
+	va_end(arg);
+	return (len);
+}
